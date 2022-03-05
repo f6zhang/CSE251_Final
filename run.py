3205f1e806a49215c96bd8646b41baf62b40e6fc
zhang <zhangfutian1@gmail.com>
2022-03-04 19:55:06 -0800

    add a line

--- run.py
+++ run.py
@@ -79,7 +79,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     
-
+    print("Use GPU: " + str(torch.cuda.is_available()))
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     torchvision.datasets.FashionMNIST(root='./data', download=True)
