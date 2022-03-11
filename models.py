d44cf5b05d58d1a434edb12f9473ded52964319f
Chen Shen <c3shen@ucsd.edu>
2022-03-10 17:05:56 -0800

    add experiment code

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from turtle import forward
 import torch
 import torch.nn as nn
 
@@ -37,3 +38,115 @@ def load_model(model, device, path='./latest_model.pt'):
     model.load_state_dict(torch.load(path)["model"])
     model.to(device)
     return model
+
+
+# Conv2d operation block, 
+class conv_block(nn.Module):
+    def __init__(self,inChannel,outChannel):
+        super(conv_block,self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(inChannel, outChannel, kernel_size=3,stride=1,padding=1,bias=True),
+            nn.BatchNorm2d(outChannel),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(outChannel, outChannel, kernel_size=3,stride=1,padding=1,bias=True),
+            nn.BatchNorm2d(outChannel),
+            nn.ReLU(inplace=True)
+        )
+
+
+    def forward(self,x):
+        x = self.conv(x)
+        return x
+
+class up_conv(nn.Module):
+    def __init__(self,inChannel,outChannel):
+        super(up_conv,self).__init__()
+        self.up = nn.Sequential(
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(inChannel,outChannel,kernel_size=3,stride=1,padding=1,bias=True),
+            nn.BatchNorm2d(outChannel),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self,x):
+        x = self.up(x)
+        return x
+
+
+class U_Net(nn.Module):
+    def __init__(self, img_ch, image_size, n_classes):
+        super(U_Net,self).__init__()
+        
+        self.UnetMaxpool = nn.MaxPool2d(kernel_size=2,stride=2)
+
+        self.Conv1 = conv_block(inChannel=img_ch,outChannel=64)
+        self.Conv2 = conv_block(inChannel=64,outChannel=128)
+        self.Conv3 = conv_block(inChannel=128,outChannel=256)
+        #self.Conv4 = conv_block(inChannel=256,outChannel=512)
+        #self.Conv5 = conv_block(inChannel=512,outChannel=1024)
+
+        #self.Up5 = up_conv(inChannel=1024,outChannel=512)
+        #self.Up_conv5 = conv_block(inChannel=1024, outChannel=512)
+
+        #self.Up4 = up_conv(inChannel=512,outChannel=256)
+        #self.Up_conv4 = conv_block(inChannel=512, outChannel=256)
+        
+        self.Up3 = up_conv(inChannel=256,outChannel=128)
+        self.Up_conv3 = conv_block(inChannel=256, outChannel=128)
+        
+        self.Up2 = up_conv(inChannel=128,outChannel=64)
+        self.Up_conv2 = conv_block(inChannel=128, outChannel=64)
+
+        #self.classifier = nn.Conv2d(64, n_class, kernel_size=1, stride=1, padding=0)
+        self.out = nn.Linear(64 * (image_size) * (image_size), n_classes)
+
+
+
+    def forward(self,x):
+        x1 = self.Conv1(x)
+        x2 = self.UnetMaxpool(x1)
+        x2 = self.Conv2(x2)      
+        x3 = self.UnetMaxpool(x2)
+        x3 = self.Conv3(x3)
+        #x4 = self.UnetMaxpool(x3)
+        #x4 = self.Conv4(x4)
+        #x5 = self.UnetMaxpool(x4)
+        #x5 = self.Conv5(x5)
+
+        #x6 = self.Up5(x5)
+        #x6 = torch.cat((x4,x6),dim=1)    
+        #x6 = self.Up_conv5(x6)  
+        #x7 = self.Up4(x6)
+        #x7 = torch.cat((x3,x7),dim=1)
+        #x7 = self.Up_conv4(x7)
+        x8 = self.Up3(x3) #self.Up3(x7)
+        x8 = torch.cat((x2,x8),dim=1)
+        x8 = self.Up_conv3(x8)
+        x9 = self.Up2(x8)
+        x9 = torch.cat((x1,x9),dim=1)
+        x9 = self.Up_conv2(x9)
+        
+        x9 = x9.view(x9.size(0), -1)       
+        output  = self.out(x9)
+        return output 
+
+class RestoreCNN(nn.Module):
+    def __init__(self, inchannel, image_size):
+        super(RestoreCNN, self).__init__()
+        self.image_size = image_size
+        self.inchannel = inchannel
+        
+        self.up1 = up_conv(inChannel=inchannel,outChannel=32)
+        self.up2 = up_conv(inChannel=32,outChannel=64)
+
+        self.maxpool = nn.MaxPool2d(kernel_size=2,stride=2)
+        self.down1 = conv_block(inChannel=64, outChannel=inchannel)
+    def forward(self, x):
+        x = self.up1(x)
+        x = self.up2(x)
+        x = self.maxpool(x)
+        x = self.down1(x)
+        output = self.maxpool(x)
+
+        return output
+
---
+++ run_restore.py
@@ -0,0 +1,140 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import IterableDataset, DataLoader,TensorDataset
+from torch import optim
+from torch.autograd import Variable
+from blur import *
+from torch.utils.data.sampler import SubsetRandomSampler
+from models import *
+import argparse
+import torchvision
+import time
+from scipy.ndimage import gaussian_filter
+
+def train(train_loader, args):
+    start_time = time.time()
+    model.train()
+
+    num_epochs = args.num_epochs
+    patient = args.patient
+    best_loss = 1<<30
+    iter_loss = 0
+    training_loss = 0
+    total_step = len(train_loader)
+
+    for epoch in range(num_epochs):
+        training_loss = 0
+        for i, (images, _) in enumerate(train_loader):
+            inputs = torch.tensor(np.array([gaussian_filter(img, sigma=1) for img in images]), device=device)
+            targets = images.to(device)
+            output = model(inputs)             
+            loss = loss_func(output, targets)
+            optimizer.zero_grad()           
+            loss.backward()    
+            optimizer.step()   
+            iter_loss += loss.item()
+            training_loss += loss.item()
+            
+            if (i+1) % 100 == 0:
+                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Time cost: {:.2f}s' 
+                       .format(epoch + 1, num_epochs, i + 1, total_step, iter_loss/100.0, time.time()-start_time))
+                iter_loss = 0.0
+        print("Training Loss is {:.4f}.".format(training_loss / total_step))
+             
+        val_loss = evaluate(valid_loader)
+        if val_loss < best_loss:
+            #save_model(model, path='./' + "restore_"+ args.data + "_" + args.data_type + "_"+'latest_model.pt')
+            torch.save(model, './' + "restore_"+ args.data + "_" + str(int(args.sigma)) +  "_"+'latest_model.pt')
+            best_loss = val_loss
+            patient = 0
+        else:
+            patient += 1
+            if patient >=5:
+                break
+
+def evaluate(data_loader):
+    model.eval()
+    total = 0
+    total_loss = 0
+    with torch.no_grad():
+        for images, _ in data_loader:
+            inputs = torch.tensor(np.array([gaussian_filter(img, sigma=1) for img in images]), device=device)
+            targets = images.to(device)
+            output = model(inputs)             
+            loss = loss_func(output, targets)
+            total += len(targets )
+            total_loss += loss.item()
+    total_loss /= len(data_loader)
+    print('Loss of the model on the evaluate images: %.4f' % total_loss)
+    
+    model.train()
+    return total_loss
+
+def test(model, data_loader):
+    model = torch.load('./' + "restore_"+ args.data + "_" + str(int(args.sigma)) +  "_"+'latest_model.pt')
+    #load_model(model, device, path='./' + args.data + "_" + args.data_type + "_"+'latest_model.pt')
+    model.eval()
+    total = 0
+    total_loss = 0
+    with torch.no_grad():
+        for images, _ in data_loader:
+            inputs = torch.tensor(np.array([gaussian_filter(img, sigma=1) for img in images]), device=device)
+            targets = images.to(device)
+            output = model(inputs)             
+            loss = loss_func(output, targets)
+            total += len(targets)
+            total_loss += loss.item()
+    total_loss /= len(data_loader)
+    print('Loss of the model on the Test images: %.4f' % total_loss)
+
+    return total_loss
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process arguments.')
+    parser.add_argument('--data', type=str, default='FashionMNIST')
+    parser.add_argument('--data_type', type=str, default='original')
+    parser.add_argument('--num_epochs', type=int,default=10)
+    parser.add_argument('--patient', type=int,  default=5)
+    parser.add_argument('--batch_size',  type=int, default=64)
+    parser.add_argument('--lr', type=float, default=0.0001)
+    parser.add_argument('--sigma', type=float, default=1.0)
+    parser.add_argument('--isRestore', type=bool, default=False)
+    args = parser.parse_args()
+
+    print("Use GPU: " + str(torch.cuda.is_available()))
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    train_data, test_data = original_data(dataset=args.data)
+
+    indices = list(range(len(train_data)))
+    np.random.shuffle(indices)
+    split = len(train_data) // 10
+
+    train_idx, valid_idx = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
+    train_loader = DataLoader(train_data, num_workers=1, batch_size=args.batch_size, sampler=train_sampler)
+    valid_loader = DataLoader(train_data, num_workers=1, batch_size=args.batch_size, sampler=valid_sampler)
+    test_loader =  DataLoader(test_data, num_workers=1, batch_size=args.batch_size)
+
+    inchannel, n, n_classes = 1, 0, 0
+    if len(train_data.data[0].shape) == 2:
+        n, _ = train_data.data[0].shape
+    elif len(train_data.data[0].shape) == 3:
+        inchannel, n , _= train_data.data[0].shape
+    try:
+        n_classes = max(train_data.targets) + 1
+    except:
+        n_classes = max(train_data.labels) + 1
+
+    
+    model = RestoreCNN(inchannel, n)
+    model.to(device)
+    optimizer = optim.Adam(model.parameters(), lr = args.lr)   
+    loss_func = nn.MSELoss()
+
+    train(train_loader, args)
+    test(model, test_loader)
